b1e28984a27101d39a95ab4b48f9a50a9f0f91d2
Frank Ngo <frankngomusic@gmail.com>
2018-03-30 14:06:54 -0700

    add components for dashboard and details

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Soda } from '../soda';
+import { SodaService } from '../soda.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
+})
+export class DashboardComponent implements OnInit {
+  sodas: Soda[] = [];
+
+  constructor(private sodaService: SodaService) { }
+
+  ngOnInit() {
+    this.getSodas();
+  }
+
+  getSodas(): void {
+    this.sodaService.getSodas()
+      .subscribe(sodas => this.sodas = sodas.slice(1, 100));
+  }
+}
