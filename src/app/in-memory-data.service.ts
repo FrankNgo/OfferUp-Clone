de0e84065193a85a36e177037bdff968b6d41bc6
Frank Ngo <Frankngomusic@gmail.com>
2018-04-04 00:22:36 -0700

    add sample items

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,16 +3,26 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const items = [
-      { id: 11, name: 'Coke', quantity: 157,background: 'https://thelibrarylife.files.wordpress.com/2012/09/twitter-background7.png' },
-      { id: 12, name: 'Fanta', quantity: 165,background: 'http://obchod.activa.cz/public/images/5748/45748/0502_3002040m.jpg' },
-      { id: 13, name: 'Sprite', quantity: 34,background:  'https://image.ec21.com/image/beveragesdirect/oimg_GC09099790_CA09099809/Fanta-Sprite-Soft-Drink-330cl-Can--24-Per-Case.jpg'},
-      { id: 14, name: 'Pepsi', quantity: 391,background:  'https://4.imimg.com/data4/ZZ/ZZ/GLADMIN-/wp-content-uploads-2015-08-pepsi-can-250x250.jpg'},
-      { id: 15, name: 'Root Beer', quantity: 719,background:  'https://www.fingereze.com/78-home_default/aw-root-beer-can-hidden-diversion-safe.jpg'},
-      { id: 16, name: 'Ginger Ale', quantity: 64,background:  'http://www.crowncoffee.net/assets/images/208402_sk_lg.jpg'},
-      { id: 17, name: '7UP', quantity: 37,background:  'https://5.imimg.com/data5/KB/CI/MY-41081178/7up-330ml-250x250.jpg'},
-      { id: 18, name: 'Lemonade', quantity: 44,background:  'https://images.chickadvisor.com/item/34646/large/default.jpg'},
-      { id: 19, name: 'Mtn.Dew', quantity: 12,background:  'https://www.officerock.com/media/catalog/product/cache/1/small_image/250x/9df78eab33525d08d6e5fb8d27136e95/M/o/MountainDew355mlCan.jpg'},
-      { id: 20, name: 'Cream Soda', quantity: 92,background:  'https://www.mykholls.com/image/cache/data/TRADE/S1W102325-250x250.jpg'}
+      { id: 11, name: '1953 Silver Certificate', quantity: 0,background: 'https://d2j6tswx2otu6e.cloudfront.net/a0MlFs5mTJk2NwbnVIZT-PVesGc=/600x1066/a9b6/a9b650f6b37d49c9bd534749cc8dffb6.jpg' },
+      { id: 12, name: 'Brand new heels', quantity: 40,background: 'https://d2j6tswx2otu6e.cloudfront.net/1153gE0m4kSa7y4KYkV-X8hR4BA=/300x400/9791/9791c94d62664c508260e3eb2e13aeaf.jpg' },
+      { id: 13, name: 'Kirkland 3L Olive oil', quantity: 15,background:  'https://d2j6tswx2otu6e.cloudfront.net/0yd5bTQLLKVptz415MU-Gc7FXPM=/600x801/29ef/29efe5febb3d4effaf9c522a719e18ae.jpg'},
+      { id: 14, name: 'Air max flyposite', quantity: 50,background:  'https://d2j6tswx2otu6e.cloudfront.net/vxvFmcag4kGSPuNsSo3h7gshUa0=/600x800/a6c2/a6c283a1c7e04782b583af25e7118edb.jpg'},
+      { id: 15, name: 'Table and 2 chairs', quantity: 55,background:  'https://d2j6tswx2otu6e.cloudfront.net/tvvxIcaNYVtBitqMjKZVnGuAxyQ=/600x450/32f3/32f38ca2e0914719bcc0a2006e798444.jpg'},
+      { id: 16, name: 'Band new nice watch', quantity: 35,background:  'https://d2j6tswx2otu6e.cloudfront.net/Z_8b7RNrF1eo_VoOVwpJFuQE7so=/600x801/3c8d/3c8d8b9672254899ab9527251d29c59c.jpg'},
+      { id: 17, name: 'Maytag washer', quantity: 140,background:  'https://d2j6tswx2otu6e.cloudfront.net/yNAyD_QI6PW7HG8jIWhuySZqB7s=/585x1040/6484/64848ea4c2aa434a96bfb10102f8ce82.jpg'},
+      { id: 18, name: 'Ford Focus SE 2009', quantity: 6000,background:  'https://d2j6tswx2otu6e.cloudfront.net/iNM22TLjYOxrK9CcikUQo-JTFS8=/600x450/99c6/99c60ff14fb44c9b8d7fac82c014cb06.jpg'},
+      { id: 19, name: 'Video game', quantity: 25,background:  'https://d2j6tswx2otu6e.cloudfront.net/9N9vhlCP8RvkjBAQ-L4uco1EG00=/600x450/b489/b48991506b414323adcce5bc36270407.jpg'},
+      { id: 20, name: 'Cream Soda', quantity: 3,background:  'https://www.mykholls.com/image/cache/data/TRADE/S1W102325-250x250.jpg'},
+      { id: 11, name: 'Brand new heels', quantity: 40,background: 'https://thelibrarylife.files.wordpress.com/2012/09/twitter-background7.png' },
+      { id: 12, name: 'Brand new heels', quantity: 40,background: 'https://d2j6tswx2otu6e.cloudfront.net/1153gE0m4kSa7y4KYkV-X8hR4BA=/300x400/9791/9791c94d62664c508260e3eb2e13aeaf.jpg' },
+      { id: 13, name: 'Kirkland 3L Olive oil', quantity: 15,background:  'https://d2j6tswx2otu6e.cloudfront.net/0yd5bTQLLKVptz415MU-Gc7FXPM=/600x801/29ef/29efe5febb3d4effaf9c522a719e18ae.jpg'},
+      { id: 14, name: 'Air max flyposite', quantity: 50,background:  'https://d2j6tswx2otu6e.cloudfront.net/vxvFmcag4kGSPuNsSo3h7gshUa0=/600x800/a6c2/a6c283a1c7e04782b583af25e7118edb.jpg'},
+      { id: 15, name: 'Table and 2 chairs', quantity: 55,background:  'https://d2j6tswx2otu6e.cloudfront.net/tvvxIcaNYVtBitqMjKZVnGuAxyQ=/600x450/32f3/32f38ca2e0914719bcc0a2006e798444.jpg'},
+      { id: 16, name: 'Band new nice watch', quantity: 35,background:  'https://d2j6tswx2otu6e.cloudfront.net/Z_8b7RNrF1eo_VoOVwpJFuQE7so=/600x801/3c8d/3c8d8b9672254899ab9527251d29c59c.jpg'},
+      { id: 17, name: 'Maytag washer', quantity: 140,background:  'https://d2j6tswx2otu6e.cloudfront.net/yNAyD_QI6PW7HG8jIWhuySZqB7s=/585x1040/6484/64848ea4c2aa434a96bfb10102f8ce82.jpg'},
+      { id: 18, name: 'Ford Focus SE 2009', quantity: 6000,background:  'https://d2j6tswx2otu6e.cloudfront.net/iNM22TLjYOxrK9CcikUQo-JTFS8=/600x450/99c6/99c60ff14fb44c9b8d7fac82c014cb06.jpg'},
+      { id: 19, name: 'Video game', quantity: 25,background:  'https://d2j6tswx2otu6e.cloudfront.net/9N9vhlCP8RvkjBAQ-L4uco1EG00=/600x450/b489/b48991506b414323adcce5bc36270407.jpg'},
+      { id: 20, name: 'Cream Soda', quantity: 3,background:  'https://www.mykholls.com/image/cache/data/TRADE/S1W102325-250x250.jpg'}
     ];
     return {items};
   }
