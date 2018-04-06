c77c93a615e617f554c13c1429373c048d1864df
Frank Ngo <Frankngomusic@gmail.com>
2018-04-06 10:41:20 -0700

    changed some css styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,22 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { ItemsComponent } from './items/items.component';
-import { ItemDetailComponent } from './item-detail/item-detail.component';
-import { ItemService } from './item.service';
-import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
-import { ItemSearchComponent } from './item-search/item-search.component';
 import { FrontPageComponent } from './front-page/front-page.component';
-
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsComponent,
-    ItemDetailComponent,
-    MessagesComponent,
-    DashboardComponent,
-    ItemSearchComponent,
-    FrontPageComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
-
+    routing
   ],
-  providers: [
-    ItemService,
-    MessageService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FrontPageComponent } from './front-page/front-page.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: FrontPageComponent
+  }
+];
+
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
